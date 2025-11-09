4df55dc6b26f147c77e660f74f3abed6d03b7847
José Danubio de Araujo <danubio.bwm@gmail.com>
2025-11-09 11:25:22 -0300

    entities

--- Domain/Entities/Courier.cs
+++ Domain/Entities/Courier.cs
@@ -1,12 +1,39 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace Domain.Entities
 {
-    public class Courier
-    {
-        
-    }
+public class Courier
+{
+[Key]
+public Guid Id { get; set; }
+
+
+[Required]
+[MaxLength(200)]
+public string Name { get; set; }
+
+
+[Required]
+[MaxLength(20)]
+public string Cnpj { get; set; }
+
+
+[Required]
+public DateTime BirthDate { get; set; }
+
+
+[Required]
+[MaxLength(50)]
+public string CnhNumber { get; set; }
+
+
+[Required]
+[MaxLength(10)]
+public string CnhCategory { get; set; }
+
+
+public string CnhImagePath { get; set; }
+}
 }
--- Domain/Entities/Motorcycle.cs
+++ Domain/Entities/Motorcycle.cs
@@ -7,6 +7,22 @@ namespace Domain.Entities
 {
     public class Motorcycle
     {
-        
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public int Year { get; set; }
+
+
+        [Required]
+        [MaxLength(200)]
+        public string Model { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public string Plate { get; set; }
+
+        public ICollection<Rental> Rentals { get; set; }
+
     }
 }
